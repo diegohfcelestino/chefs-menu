9d6eeaae248698779129498f783e2abad5b62310
diegohfcelestino <diegohfcelestino@gmail.com>
2024-01-20 23:14:35 -0300

    Iniciando documentação com Swagger

--- api-restaurante/index.js
+++ api-restaurante/index.js
@@ -6,6 +6,7 @@ import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import restaurantRoute from "./src/routes/restaurant.route.js";
 import menuRoute from "./src/routes/menu.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 dotenv.config();
 
@@ -17,5 +18,6 @@ app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/restaurant", restaurantRoute);
 app.use("/menu", menuRoute);
+app.use("/documentation", swaggerRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
--- api-restaurante/src/routes/user.route.js
+++ api-restaurante/src/routes/user.route.js
@@ -1,13 +1,13 @@
-import { Router } from "express"
-import { authMiddleware } from "../middlewares/auth.middleware.js"
-import userController from "../controllers/user.controller.js"
-import { validId, validUser } from "../middlewares/global.middlewares.js"
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import userController from "../controllers/user.controller.js";
+import { validId, validUser } from "../middlewares/global.middlewares.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/", userController.create)
-router.get("/", userController.findAll)
-router.get("/:id", validId, validUser, userController.findById)
-router.patch("/:id", authMiddleware, validId, validUser, userController.update)
+router.post("/", userController.create);
+router.get("/", userController.findAll);
+router.get("/:id", validId, validUser, userController.findById);
+router.patch("/:id", authMiddleware, validId, validUser, userController.update);
 
-export default router
+export default router;
